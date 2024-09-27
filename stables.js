deca29009e01b4b5aa2d267055a3dddff4e54595
THE3NAT <Nathan03Irwin@gmail.com>
2024-09-27 16:08:51 -0700

    updating stables with lab work

--- stables.js
+++ stables.js
@@ -1,9 +1,9 @@
 let horseName = "Cuthulador";
-console.log(horseName);
+//console.log(horseName);
 let horseAge = 27;
-console.log(horseAge);
+//console.log(horseAge);
 let isHorseInside = true;
-console.log(isHorseInside);
+//console.log(isHorseInside);
 
 //Lesson 4 code
 console.log(typeof "123") //this is a string because of the ""
@@ -12,4 +12,6 @@ let y = 1000;
 let z = 10;
 let a = 1.79769313486231 //What is the max number??
 const STABLE_MONTHLY_FEE = x*x*x*x*x*x*x*x*x*x*x*x*x*x*x*x*x*x*x*x*x*x*x*x*x*y*y*z*z*a;
-console.log(STABLE_MONTHLY_FEE)
+console.log("The stable will cost $" + (STABLE_MONTHLY_FEE*0.9) + "a month. This is 10% off")
+let horseNickname = "Tree Man"
+console.log(horseNickname)
